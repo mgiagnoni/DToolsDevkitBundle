ff9df1effcb4889c19337598db76734bc9d99a63
mgiagnoni <gimassimo@gmail.com>
2011-04-25 11:30:36 +0200

    Added missing copyright headers

--- Command/AddLicenseCommand.php
+++ Command/AddLicenseCommand.php
@@ -1,4 +1,12 @@
 <?php
+
+/**
+ * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ *
+ * This source file is subject to the MIT license included
+ * with this source code (Resources/meta/LICENSE).
+ */
+
 namespace DTools\DevkitBundle\Command;
 
 use Symfony\Component\Console\Input\InputArgument;
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Copyright 2011 Massimo Giagnoni <gimassimo@gmail.com>
+ *
+ * This source file is subject to the MIT license included
+ * with this source code (Resources/meta/LICENSE).
+ */
+
 require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
